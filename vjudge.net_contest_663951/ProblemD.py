09b7776e32b8cead4db9790165447724d4041a6a
Md Safayet Al Hossain <31356820+hossainel@users.noreply.github.com>
2024-10-19 09:32:09 +0600

    Create ProblemD.py

---
+++ vjudge.net_contest_663951/ProblemD.py
@@ -0,0 +1,20 @@
+import math
+
+def line(a,b,c,d): return math.sqrt((a-c)**2+(b-d)**2)
+    
+
+def line_pos(a,b,c,d,e,f):
+    A = d-b
+    B = a-c
+    C = 0 - b * B - A * a
+    dist = (A*e + B*f + C)/math.sqrt(A*A+B*B)
+    if dist==0.0: return "TOUCH"
+    elif dist<0: return "LEFT"
+    else: return "RIGHT"
+    return dist
+
+for _ in range(int(input())):
+    a,b,c,d,e,f = input().split()
+    y = line_pos(int(a),int(b),int(c),int(d),int(e),int(f))
+    print(y)
+    
